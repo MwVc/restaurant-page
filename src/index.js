d8a2ec62f1d38b1a354aad296fdcfb7648d60bc8
Victor Mwadime <victormwadime@gmail.com>
2023-12-20 13:57:06 +0300

    create navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { header } from "./modules/home";
 
-document.body.appendChild(header());
+const headerElement = header();
+
+document.body.appendChild(headerElement);
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,11 +1,44 @@
+// HEADER
+//  heading
+//  nav
+//    div
+//      ul
+//        li
+//        li
+//        li
+
 const header = function () {
-  //create header element
+  //create html element
   const headerElement = document.createElement("header");
   const heading = document.createElement("h1");
+
   heading.textContent = "MwVc";
   headerElement.appendChild(heading);
+  headerElement.appendChild(navigation());
 
   return headerElement;
+};
+
+const navigation = function () {
+  const navlist = ["HOME", "MENU", "CONTACTS"];
+
+  const navElement = document.createElement("nav");
+
+  const navContainer = document.createElement("div");
+  navContainer.classList.add("nav-container");
+
+  const ulElement = document.createElement("ul");
+
+  for (let i = 0; i < navlist.length; i++) {
+    let listItem = document.createElement("li");
+    listItem.textContent = navlist[i];
+    navContainer.appendChild(listItem);
+  }
+
+  ulElement.appendChild(navContainer);
+  navElement.appendChild(navContainer);
+
+  return navElement;
 };
 
 export { header };
