787f39b57df0f8f31b30ba8212edc77c7aa7aed2
Victor Mwadime <victormwadime@gmail.com>
2023-12-27 13:01:33 +0300

    create menu page cards

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,4 @@
+import { container } from "webpack";
 import { generateMenuItems } from "./home";
 
 export default function loadMenu() {
@@ -38,6 +39,82 @@ const generateNav = function () {
   container.appendChild(menuContainer);
   navigationBar.appendChild(container);
   return navigationBar;
+};
+
+const menuCards = function () {
+  const section = document.createElement("section");
+  const cardsContainer = document.createElement("div");
+  const menuArray = [
+    {
+      name: "Jollof Rice",
+      description:
+        "A popular West African dish made with rice, tomatoes, onions, and a blend of spices, often served with grilled or fried chicken, fish, or other protein.",
+      image: "../img/jollof.jpg",
+    },
+    {
+      name: "Bobotie",
+      description:
+        "A South African dish consisting of spiced minced meat baked with an egg-based topping, typically served with yellow rice and chutney.",
+      image: "../img/Bobotie.jpeg",
+    },
+    {
+      name: "Injera",
+      description:
+        "A sourdough flatbread that is a staple in Ethiopian and Eritrean cuisine, usually served with a variety of stews and dishes.",
+      image: "../img/Injera.jpg",
+    },
+    {
+      name: "Suya",
+      description:
+        "A popular Nigerian street food, suya is skewered and grilled spicy meat (often beef or chicken) served with sliced onions, tomatoes, and pepper sauce.",
+      image: "../img/suya.jpeg",
+    },
+    {
+      name: "Bunny Chow",
+      description:
+        "Originating from South Africa, Bunny Chow is a fast food dish consisting of a hollowed-out loaf of bread filled with curry, typically chicken or mutton curry.",
+      image: "../image/bunny_chow.jpg",
+    },
+    {
+      name: "Dibi",
+      description:
+        "A Senegalese dish featuring grilled lamb skewers marinated in a flavorful blend of spices, often served with mustard sauce and onions.",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/5ed13dd3465af021e2c1342b/a8e83567-1a5f-4ff1-b4e6-33e9af1fd54d/714EC122-A607-4530-827C-AD33B96BC2D9.jpeg?format=1500w",
+    },
+  ];
+
+  function cards() {
+    for (let i = 0; i < menuArray.length; i++) {
+      const card = document.createElement("div");
+      const img = document.createElement("img");
+      img.setAttribute("src", `${menuArray[i].image}`);
+      img.setAttribute("alt", "Avatar");
+      img.setAttribute("style", "width: 100%");
+
+      const container = document.createElement("div");
+      container.classList.add("container");
+
+      const heading = document.createElement("h4");
+      const description = document.createElement("p");
+
+      heading.textContent = menuArray[i].name;
+      description.textContent = menuArray[i].description;
+
+      card.appendChild(img);
+      card.appendChild(container);
+      container.appendChild(heading);
+      container.appendChild(description);
+
+      return card;
+    }
+  }
+
+  const card = cardsContainer.classList.add("cards-container");
+
+  section.appendChild(cardsContainer);
+
+  return section;
 };
 
 const generateFooter = function () {
